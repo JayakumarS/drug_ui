88e5ad343ed5ce732fc8cf89160415cf92214f9a
StephenVivek <cvivek7080@gmail.com>
2022-07-12 12:52:21 +0530

    company master screen

--- src/app/setup/company-master/add-company-master/add-company-master.component.ts
+++ src/app/setup/company-master/add-company-master/add-company-master.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { DetailRowComponent } from 'src/app/crm/customer-master/detail-row/detail-row.component';
+import { AuthService } from 'src/app/auth/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CustomerMasterService } from 'src/app/crm/customer-master/customer-master.service'; 
+import { HttpServiceService } from 'src/app/auth/http-service.service';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { CustomerMaster } from 'src/app/crm/customer-master/customer-master.model';
 
 @Component({
   selector: 'app-add-company-master',
@@ -6,10 +15,280 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-company-master.component.sass']
 })
 export class AddCompanyMasterComponent implements OnInit {
+  companyAuthorizedClassesForm: FormGroup;
 
-  constructor() { }
+  docForm: FormGroup;
+  hide3 = true;
+  agree3 = false;
+  dataarray=[];
+  cusMasterData =[];
+  customerMaster:CustomerMaster;
+  detailRowData = new DetailRowComponent;
+  requestId: number;
+  edit: boolean=false;
+  constructor(private fb: FormBuilder,private authService: AuthService,public router: Router,
+    private customerMasterService:CustomerMasterService,private httpService: HttpServiceService
+    ,private snackBar: MatSnackBar,public route: ActivatedRoute) {
+    this.docForm = this.fb.group({
+      wholesalerCode: [""],
+      wholesalerPolicyCode: ["", [Validators.required]],
+      wholesalerName: ["", [Validators.required]],
+      wholesalerExpiryPacket: [""],
+      wholesalerEmailID: ["", [Validators.required]],
+      wholesalerAllowOverride: [""],
+      wholesalerDepartment: ["", [Validators.required]],
+      wholesalerStreet: ["", [Validators.required]],
+      wholesalerCity: ["", [Validators.required]],
+      wholesalerState: ["", [Validators.required]],
+      wholesalerZipCode:["", [Validators.required]],
+      wholesalerPhoneNo: ["", [Validators.required]],
+      wholesalerTollFreeNo: ["", [Validators.required]],
+      wholesalerFax: ["", [Validators.required]],
+      wholesalerPhone: ["", [Validators.required]],
 
-  ngOnInit(): void {
+      companyCode: [""],
+companyName:["", [Validators.required]],
+companyDba:[""],
+companyStreet:["", [Validators.required]],
+companyCity:["", [Validators.required]],
+companyState:["", [Validators.required]],
+companyPincode:["", [Validators.required]],
+companyPhone:["", [Validators.required]],
+companyFax:["", [Validators.required]],
+companyContact:["", [Validators.required]],
+companyEmailID:["", [Validators.required]],
+
+authorizedClasses:[""],
+
+
+companyFacilityType:["", [Validators.required]],
+
+defNumber:["", [Validators.required]],
+defExpirationDate:["", [Validators.required]],
+
+issuesCreditsName:["", [Validators.required]],
+
+issuesCreditsStreet:["", [Validators.required]],
+issuesCreditsCity:["", [Validators.required]],
+issuesCreditsState:["", [Validators.required]],
+issuesCreditsZipCode:["", [Validators.required]],
+issuesCreditsPhone:["", [Validators.required]],
+
+generalInfroWacAwapMyprice:["", [Validators.required]],
+generalInfroWacAwapPer:[""],
+
+myWholesalerPolicyType:["", [Validators.required]],
+myWholesalerPolicyMonths:[""],
+myWholesalerCpp:["false", [Validators.required]],
+cppServiceRate:["", [Validators.required]],
+cppShippingRate:["", [Validators.required]],
+cppNoOfChecks:["", [Validators.required]],
+    });
+
+    this.companyAuthorizedClassesForm = this.fb.group({
+      companyAuthorizedClasses2: false,
+      companyAuthorizedClasses2N: false,
+      companyAuthorizedClasses3: false,
+      companyAuthorizedClasses3N: false,
+      companyAuthorizedClasses4: false,
+      companyAuthorizedClasses5: false,
+    });
+
   }
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      if(params.id!=undefined && params.id!=0){
+       this.requestId = params.id;
+       this.edit=true;
+       //For User login Editable mode
+       this.fetchDetails(this.requestId) ;
+      }
+     });
+  }
+  onSubmit() {
+    
+if (this.docForm.valid) {
+  this.docForm.patchValue({
+    'authorizedClasses': this.companyAuthorizedClassesForm.value.companyAuthorizedClasses2+','+this.companyAuthorizedClassesForm.value.companyAuthorizedClasses2N+','+this.companyAuthorizedClassesForm.value.companyAuthorizedClasses3+','+this.companyAuthorizedClassesForm.value.companyAuthorizedClasses3N+','+this.companyAuthorizedClassesForm.value.companyAuthorizedClasses4+','+this.companyAuthorizedClassesForm.value.companyAuthorizedClasses5
+  });
+
+    this.customerMaster = this.docForm.value;
+    console.log(this.customerMaster);
+    
+    this.customerMasterService.addCustomerMaster(this.customerMaster);
+    this.showNotification(
+      "snackbar-success",
+      "Add Record Successfully...!!!",
+      "bottom",
+      "center"
+    );
+    this.router.navigate(['/crm/customerMaster/listCustomer']);
+  }
+  }
+  fetchDetails(cusCode: any): void {
+    this.httpService.get(this.customerMasterService.editCustomermaster+"?customer="+cusCode).subscribe((res: any)=> {
+      console.log(cusCode);
+
+      this.docForm.patchValue({
+        'wholesalerCode' : res.customerMasterBean.wholesalerCode,
+        'wholesalerPolicyCode' : res.customerMasterBean.wholesalerPolicyCode,
+        'wholesalerName' : res.customerMasterBean.wholesalerName,
+        'wholesalerExpiryPacket' : res.customerMasterBean.wholesalerExpiryPacket,
+        'wholesalerEmailID' : res.customerMasterBean.wholesalerEmailID,
+        'wholesalerAllowOverride' : res.customerMasterBean.wholesalerAllowOverride,
+        'wholesalerDepartment' : res.customerMasterBean.wholesalerDepartment,
+        'wholesalerStreet' : res.customerMasterBean.wholesalerStreet,
+        'wholesalerCity' : res.customerMasterBean.wholesalerCity,
+        'wholesalerState' : res.customerMasterBean.wholesalerState,
+        'wholesalerZipCode' : res.customerMasterBean.wholesalerZipCode,
+        'wholesalerPhoneNo' : res.customerMasterBean.wholesalerPhoneNo,
+        'wholesalerTollFreeNo' : res.customerMasterBean.wholesalerTollFreeNo,
+        'wholesalerFax' : res.customerMasterBean.wholesalerFax,
+        'wholesalerPhone' : res.customerMasterBean.wholesalerPhone,
+        
+        'companyCode': res.customerMasterBean.companyCode,
+        'companyName': res.customerMasterBean.companyName,
+        'companyDba': res.customerMasterBean.companyDba,
+        'companyStreet': res.customerMasterBean.companyStreet,
+        'companyCity': res.customerMasterBean.companyCity,
+        'companyState': res.customerMasterBean.companyState,
+        'companyPincode': res.customerMasterBean.companyPincode,
+        'companyPhone': res.customerMasterBean.companyPhone,
+        'companyFax': res.customerMasterBean.companyFax,
+        'companyContact': res.customerMasterBean.companyContact,
+        'companyEmailID': res.customerMasterBean.companyEmailID,
+        'companyFacilityType': res.customerMasterBean.companyFacilityType,
+        
+        'defNumber': res.customerMasterBean.defNumber,
+        'defExpirationDate': res.customerMasterBean.defExpirationDate,
+        
+        'issuesCreditsName': res.customerMasterBean.issuesCreditsName,
+        
+        'issuesCreditsStreet': res.customerMasterBean.issuesCreditsStreet,
+        'issuesCreditsCity': res.customerMasterBean.issuesCreditsCity,
+        'issuesCreditsState': res.customerMasterBean.issuesCreditsState,
+        'issuesCreditsZipCode': res.customerMasterBean.issuesCreditsZipCode,
+        'issuesCreditsPhone': res.customerMasterBean.issuesCreditsPhone,
+        
+        'generalInfroWacAwapMyprice': res.customerMasterBean.generalInfroWacAwapMyprice,
+        'generalInfroWacAwapPer':res.customerMasterBean.generalInfroWacAwapPer,
+        
+        'myWholesalerPolicyType': res.customerMasterBean.myWholesalerPolicyType.toString(),
+        'myWholesalerPolicyMonths':res.customerMasterBean.myWholesalerPolicyMonths,
+        'myWholesalerCpp': res.customerMasterBean.myWholesalerCpp.toString(),
+        'cppServiceRate': res.customerMasterBean.cppServiceRate,
+        'cppShippingRate': res.customerMasterBean.cppShippingRate,
+        'cppNoOfChecks': res.customerMasterBean.cppNoOfChecks
+     })
+
+ var companyAuthorizedSplitedList=res.customerMasterBean.authorizedClasses.split(',');
+
+     this.companyAuthorizedClassesForm.patchValue({
+      'companyAuthorizedClasses2': this.getBoolean(companyAuthorizedSplitedList[0]),
+      'companyAuthorizedClasses2N': this.getBoolean(companyAuthorizedSplitedList[1]),
+      'companyAuthorizedClasses3': this.getBoolean(companyAuthorizedSplitedList[2]),
+      'companyAuthorizedClasses3N': this.getBoolean(companyAuthorizedSplitedList[3]),
+      'companyAuthorizedClasses4': this.getBoolean(companyAuthorizedSplitedList[4]),
+      'companyAuthorizedClasses5': this.getBoolean(companyAuthorizedSplitedList[5]),  
+   })
+
+      },
+      (err: HttpErrorResponse) => {
+         // error code here
+      }
+    );
+  }
+
+  
+  update(){
+    if (this.docForm.valid) {
+    this.docForm.patchValue({
+      'authorizedClasses': this.companyAuthorizedClassesForm.value.companyAuthorizedClasses2+','+this.companyAuthorizedClassesForm.value.companyAuthorizedClasses2N+','+this.companyAuthorizedClassesForm.value.companyAuthorizedClasses3+','+this.companyAuthorizedClassesForm.value.companyAuthorizedClasses3N+','+this.companyAuthorizedClassesForm.value.companyAuthorizedClasses4+','+this.companyAuthorizedClassesForm.value.companyAuthorizedClasses5
+    });
+
+    this.customerMaster = this.docForm.value;
+    this.customerMasterService.customerMasterUpdate(this.customerMaster);
+    this.showNotification(
+      "snackbar-success",
+      "Edit Record Successfully...!!!",
+      "bottom",
+      "center"
+    );
+    this.router.navigate(['/crm/customerMaster/listCustomer']);
+
+  }
+  }
+  reset(){}
+
+  addRow(){
+    this.detailRowData=new DetailRowComponent()
+    this.dataarray.push(this.detailRowData)
+
+  }
+  removeRow(index){
+    this.dataarray.splice(index, 1);
+  }
+  onCancel(){
+    this.router.navigate(['/crm/customerMaster/listCustomer']);
+   }
+   autoFillClick(){
+
+    this.docForm.patchValue({
+      'issuesCreditsStreet': this.docForm.value.companyStreet,
+      'issuesCreditsCity': this.docForm.value.companyCity,
+      'issuesCreditsState': this.docForm.value.companyState,
+      'issuesCreditsZipCode': this.docForm.value.companyPincode,
+      'issuesCreditsPhone': this.docForm.value.companyPhone,     
+   })
+
+   }
+
+
+
+   keyPressName(event: any) {
+    const pattern = /[A-Z,a-z 0-9]/;
+    const inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+  keyPressNumberDouble(event: any) {
+    const pattern = /[0-9.]/;
+    const inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
+  keyPressNumberInt(event: any) {
+    const pattern = /[0-9]/;
+    const inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
+   showNotification(colorName, text, placementFrom, placementAlign) {
+    this.snackBar.open(text, "", {
+      duration: 3000,
+      verticalPosition: placementFrom,
+      horizontalPosition: placementAlign,
+      panelClass: colorName,
+    });
+  }
+
+  getBoolean(value){
+    switch(value){
+         case true:
+         case "true":
+         case 1:
+         case "1":
+         case "on":
+         case "yes":
+             return true;
+         default: 
+             return false;
+     }
+    }
 
 }
--- src/app/setup/company-master/list-company-master/list-company-master.component.ts
+++ src/app/setup/company-master/list-company-master/list-company-master.component.ts
@@ -1,15 +1,269 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { CustomerMasterService } from 'src/app/crm/customer-master/customer-master.service'; 
+import { CustomerMaster } from 'src/app/crm/customer-master/customer-master.model';
+import { HttpClient } from "@angular/common/http";
+import { MatDialog } from "@angular/material/dialog";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { DataSource } from "@angular/cdk/collections";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { MatMenuTrigger } from "@angular/material/menu";
+import { BehaviorSubject, fromEvent, merge, Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { SelectionModel } from "@angular/cdk/collections";
+import { UnsubscribeOnDestroyAdapter } from "src/app/shared/UnsubscribeOnDestroyAdapter";
+import { serverLocations } from 'src/app/auth/serverLocations';
+import { HttpServiceService } from 'src/app/auth/http-service.service';
+import { Router } from '@angular/router';
+import { DeleteCustomerComponent } from 'src/app/crm/customer-master/list-customer/delete-customer/delete-customer.component'; 
+import { CalculatorCustomerComponent } from 'src/app/crm/customer-master/list-customer/calculator-customer/calculator-customer.component'; 
 
 @Component({
   selector: 'app-list-company-master',
   templateUrl: './list-company-master.component.html',
   styleUrls: ['./list-company-master.component.sass']
 })
-export class ListCompanyMasterComponent implements OnInit {
+export class ListCompanyMasterComponent extends UnsubscribeOnDestroyAdapter implements OnInit {
+  displayedColumns = [
 
-  constructor() { }
+    "companyName",
+    "companyEmailID",
+    
+    "companyCity", 
+   "companyState",
+   "actions"
+  ];
 
-  ngOnInit(): void {
+  dataSource: ExampleDataSource | null;
+  exampleDatabase: CustomerMasterService | null;
+  selection = new SelectionModel<CustomerMaster>(true, []);
+  index: number;
+  id: number;
+  customerMaster: CustomerMaster | null;
+  constructor(
+    public httpClient: HttpClient,
+    public dialog: MatDialog,
+    public customerMasterService: CustomerMasterService,
+    private snackBar: MatSnackBar,
+    private serverUrl:serverLocations,
+    private httpService:HttpServiceService,
+    public router: Router,
+  ) {
+    super();
   }
 
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild("filter", { static: true }) filter: ElementRef;
+  @ViewChild(MatMenuTrigger)
+  contextMenu: MatMenuTrigger;
+  contextMenuPosition = { x: "0px", y: "0px" };
+
+  ngOnInit(): void {
+    this.loadData();
+
+    // if (!localStorage.getItem('foo')) { 
+    //   localStorage.setItem('foo', 'no reload') 
+    //   location.reload() 
+    // } else {
+    //   localStorage.removeItem('foo') 
+    // }
+  }
+
+  refresh(){
+    this.loadData();
+  }
+
+  public loadData() {
+    this.exampleDatabase = new CustomerMasterService(this.httpClient,this.serverUrl,this.httpService);
+    this.dataSource = new ExampleDataSource(
+      this.exampleDatabase,
+      this.paginator,
+      this.sort
+    );
+    this.subs.sink = fromEvent(this.filter.nativeElement, "keyup").subscribe(
+      () => {
+        if (!this.dataSource) {
+          return;
+        }
+        this.dataSource.filter = this.filter.nativeElement.value;
+      }
+    );
+  }
+
+
+  editCall(row) {
+    this.router.navigate(['/crm/customerMaster/addCustomer/'+ row.companyCode]);
+  }
+
+  deleteItem(row){ 
+    this.id = row.companyCode;
+    let tempDirection;
+    if (localStorage.getItem("isRtl") === "true") {
+      tempDirection = "rtl";
+    } else {
+      tempDirection = "ltr";
+    }
+    const dialogRef = this.dialog.open(DeleteCustomerComponent, {
+      height: "270px",
+      width: "400px",
+      data: row,
+      direction: tempDirection,
+    });
+    this.subs.sink = dialogRef.afterClosed().subscribe((data) => {
+      
+      this.loadData();
+        this.showNotification(
+          "snackbar-success",
+          "Delete Record Successfully...!!!",
+          "bottom",
+          "center"
+        );
+      
+      // else{
+      //   this.showNotification(
+      //     "snackbar-danger",
+      //     "Error in Delete....",
+      //     "bottom",
+      //     "center"
+      //   );
+      // }
+    });
+
+  }
+
+  calculator(){
+
+    const dialogRef = this.dialog.open(CalculatorCustomerComponent, {
+      height: "430px",
+      width: "390px",
+  
+    });
+    this.subs.sink = dialogRef.afterClosed().subscribe((data) => {
+      
+      // this.loadData();
+      //   this.showNotification(
+      //     "snackbar-success",
+      //     "Delete Record Successfully...!!!",
+      //     "bottom",
+      //     "center"
+      //   );
+      
+      // else{
+      //   this.showNotification(
+      //     "snackbar-danger",
+      //     "Error in Delete....",
+      //     "bottom",
+      //     "center"
+      //   );
+      // }
+    });
+    
+  }
+
+  showNotification(colorName, text, placementFrom, placementAlign) {
+    this.snackBar.open(text, "", {
+      duration: 2000,
+      verticalPosition: placementFrom,
+      horizontalPosition: placementAlign,
+      panelClass: colorName,
+    });
+  }
+
+// context menu
+  onContextMenu(event: MouseEvent, item: CustomerMaster) {
+    event.preventDefault();
+    this.contextMenuPosition.x = event.clientX + "px";
+    this.contextMenuPosition.y = event.clientY + "px";
+    this.contextMenu.menuData = { item: item };
+    this.contextMenu.menu.focusFirstItem("mouse");
+    this.contextMenu.openMenu();
+  }
+}
+
+export class ExampleDataSource extends DataSource<CustomerMaster> {
+  filterChange = new BehaviorSubject("");
+  get filter(): string {
+    return this.filterChange.value;
+  }
+  set filter(filter: string) {
+    this.filterChange.next(filter);
+  }
+  filteredData: CustomerMaster[] = [];
+  renderedData: CustomerMaster[] = [];
+  constructor(
+    public exampleDatabase: CustomerMasterService,
+    public paginator: MatPaginator,
+    public _sort: MatSort
+  ) {
+    super();
+    // Reset to the first page when the user changes the filter.
+    this.filterChange.subscribe(() => (this.paginator.pageIndex = 0));
+  }
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<CustomerMaster[]> {
+    // Listen for any changes in the base data, sorting, filtering, or pagination
+    const displayDataChanges = [
+      this.exampleDatabase.dataChange,
+      this._sort.sortChange,
+      this.filterChange,
+      this.paginator.page,
+    ];
+    this.exampleDatabase.getAllCustomers();
+    return merge(...displayDataChanges).pipe(
+      map(() => {
+        // Filter data
+        this.filteredData = this.exampleDatabase.data
+          .slice()
+          .filter((customerMaster: CustomerMaster) => {
+            const searchStr = (
+              customerMaster.companyName +
+              customerMaster.companyEmailID +
+              customerMaster.companyState +
+              customerMaster.companyCity 
+            ).toLowerCase();
+            return searchStr.indexOf(this.filter.toLowerCase()) !== -1;
+          });
+        // Sort filtered data
+        const sortedData = this.sortData(this.filteredData.slice());
+        // Grab the page's slice of the filtered sorted data.
+        const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+        this.renderedData = sortedData.splice(
+          startIndex,
+          this.paginator.pageSize
+        );
+        return this.renderedData;
+      })
+    );
+  }
+  disconnect() {}
+  /** Returns a sorted copy of the database data. */
+  sortData(data: CustomerMaster[]): CustomerMaster[] {
+    if (!this._sort.active || this._sort.direction === "") {
+      return data;
+    }
+    return data.sort((a, b) => {
+      let propertyA: number | string = "";
+      let propertyB: number | string = "";
+      switch (this._sort.active) {
+        case "companyName":
+          [propertyA, propertyB] = [a.companyName, b.companyName];
+          break;
+        case "companyEmailID":
+          [propertyA, propertyB] = [a.companyEmailID, b.companyEmailID];
+          break;
+        case "companyState":
+          [propertyA, propertyB] = [a.companyState, b.companyState];
+          break;
+        case "companyCity":
+          [propertyA, propertyB] = [a.companyCity, b.companyCity];
+          break;
+      }
+      const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
+      const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
+      return (
+        (valueA < valueB ? -1 : 1) * (this._sort.direction === "asc" ? 1 : -1)
+      );
+    });
+  }
 }
