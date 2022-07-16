49103f8ae68b1f7faabae1cbb4c243c565a228d1
Gokulnath22 <gokulnath@paragondynamics.in>
2022-07-16 21:02:38 +0530

    Update common.service.ts

--- src/app/common-service/common.service.ts
+++ src/app/common-service/common.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
 import * as moment from "moment";
-<<<<<<< Updated upstream
-import { serverLocations } from '../auth/serverLocations';
-=======
 import { BehaviorSubject } from "rxjs";
 import { HttpClient, HttpErrorResponse,HttpHeaders } from "@angular/common/http";
 import { UnsubscribeOnDestroyAdapter } from "src/app/shared/UnsubscribeOnDestroyAdapter";
@@ -11,42 +8,27 @@ import { TokenStorageService } from 'src/app/auth/token-storage.service';
 import { AuthLoginInfo } from 'src/app/auth/login-info';
 import { serverLocations } from 'src/app/auth/serverLocations';
 import { HttpServiceService } from 'src/app/auth/http-service.service';
->>>>>>> Stashed changes
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
-<<<<<<< Updated upstream
-  constructor(
-
-    private serverUrl:serverLocations
-  ) { }
-=======
 @Injectable({
   providedIn: 'root'
 })
-
 export class CommonService extends UnsubscribeOnDestroyAdapter {
 
+  // Temporarily stores data from dialogs
+  dialogData: any;
   constructor(private httpClient: HttpClient,private serverUrl:serverLocations,private httpService:HttpServiceService) {
     super();
   }
->>>>>>> Stashed changes
 
   getDate(date): any {
     return moment(date).format('DD/MM/YYYY');
   }
-<<<<<<< Updated upstream
-  public getCompanyMasterDropdownList = `${this.serverUrl.apiServerAddress}api/auth/app/commonServices/getCompanyMasterDropdownList`;
+  public getcompanyMasterDropdownList = `${this.serverUrl.apiServerAddress}api/auth/app/commonServices/getCompanyMasterDropdownList`;
   public getdebitMemoDropdownList= `${this.serverUrl.apiServerAddress}api/auth/app/commonServices/getDebitMemoDropdownList`;
  
 
-=======
-  
-  public getcompanyMasterDropdownList = `${this.serverUrl.apiServerAddress}api/auth/app/commonServices/getCompanyMasterDropdownList`;
-  public getdebitMemoDropdownList= `${this.serverUrl.apiServerAddress}api/auth/app/commonServices/getDebitMemoDropdownList`;
-
-  
->>>>>>> Stashed changes
 }
