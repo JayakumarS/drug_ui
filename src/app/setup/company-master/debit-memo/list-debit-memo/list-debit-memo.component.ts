96ce25f5371cfff6ebd1bf57e932fd12041f903f
Gokulnath22 <gokulnath@paragondynamics.in>
2022-07-18 15:29:56 +0530

    DebitMemoResult

--- src/app/setup/company-master/debit-memo/list-debit-memo/list-debit-memo.component.ts
+++ src/app/setup/company-master/debit-memo/list-debit-memo/list-debit-memo.component.ts
@@ -34,7 +34,8 @@ export class ListDebitMemoComponent extends UnsubscribeOnDestroyAdapter implemen
   ];
   companyList =[];
   debitMemoList =[];
-  dataSource: ExampleDataSource | null;
+  listDebitMemo =[];
+  
   exampleDatabase: DebitmemoService | null;
   selection = new SelectionModel<DebitMemo>(true, []);
   index: number;
@@ -101,31 +102,26 @@ export class ListDebitMemoComponent extends UnsubscribeOnDestroyAdapter implemen
         }
         );
 
-
-        this.loadData();
+        setTimeout(() => {
+        this.searchData();
+      }, 700);
   }
 
-  refresh(){
-    this.loadData();
-  }
 
-  public loadData() {
-    this.exampleDatabase = new DebitmemoService(this.httpClient,this.serverUrl,this.httpService);
-    this.dataSource = new ExampleDataSource(
-      this.exampleDatabase,
-      this.paginator,
-      this.sort
-    );
-    this.subs.sink = fromEvent(this.filter.nativeElement, "keyup").subscribe(
-      () => {
-        if (!this.dataSource) {
-          return;
-        }
-        this.dataSource.filter = this.filter.nativeElement.value;
+  searchData(){
+    this.httpService.post<any>(this.debitmemoService.getAllMasters, this.docForm.value).subscribe(
+      (data) => {
+        this.listDebitMemo= data.listDebitMemo;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.name + " " + error.message);
       }
-    );
+      );
   }
 
+
+
+ 
 
   editCall(row) {
      
@@ -143,7 +139,7 @@ export class ListDebitMemoComponent extends UnsubscribeOnDestroyAdapter implemen
    });
    this.subs.sink = dialogRef.afterClosed().subscribe((data) => {
      
-     this.loadData();
+     this.searchData();
        this.showNotification(
          "snackbar-success",
          "Record Saved Successfully...!!!",
@@ -181,7 +177,7 @@ export class ListDebitMemoComponent extends UnsubscribeOnDestroyAdapter implemen
    });
    this.subs.sink = dialogRef.afterClosed().subscribe((data) => {
      
-     this.loadData();
+     this.searchData();
        this.showNotification(
          "snackbar-success",
          "Record Saved Successfully...!!!",
@@ -194,25 +190,9 @@ export class ListDebitMemoComponent extends UnsubscribeOnDestroyAdapter implemen
 
   }
 
-  onOk() {
-    
-  }
+
 
   
-
-  // calculator(){
-
-  //   const dialogRef = this.dialog.open(CalculatorCustomerComponent, {
-  //     height: "430px",
-  //     width: "390px",
-  
-  //   });
-  //   this.subs.sink = dialogRef.afterClosed().subscribe((data) => {
-  
-  //   });
-    
-  // }
-
   showNotification(colorName, text, placementFrom, placementAlign) {
     this.snackBar.open(text, "", {
       duration: 2000,
@@ -230,92 +210,5 @@ export class ListDebitMemoComponent extends UnsubscribeOnDestroyAdapter implemen
     this.contextMenu.menuData = { item: item };
     this.contextMenu.menu.focusFirstItem("mouse");
     this.contextMenu.openMenu();
-  }
-}
-
-export class ExampleDataSource extends DataSource<DebitMemo> {
-  filterChange = new BehaviorSubject("");
-  get filter(): string {
-    return this.filterChange.value;
-  }
-  set filter(filter: string) {
-    this.filterChange.next(filter);
-  }
-  filteredData: DebitMemo[] = [];
-  renderedData: DebitMemo[] = [];
-  constructor(
-    public exampleDatabase: DebitmemoService,
-    public paginator: MatPaginator,
-    public _sort: MatSort
-  ) {
-    super();
-    // Reset to the first page when the user changes the filter.
-    this.filterChange.subscribe(() => (this.paginator.pageIndex = 0));
-  }
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<DebitMemo[]> {
-    // Listen for any changes in the base data, sorting, filtering, or pagination
-    const displayDataChanges = [
-      this.exampleDatabase.dataChange,
-      this._sort.sortChange,
-      this.filterChange,
-      this.paginator.page,
-    ];
-    this.exampleDatabase.getAllCompany();
-    return merge(...displayDataChanges).pipe(
-      map(() => {
-        // Filter data
-        this.filteredData = this.exampleDatabase.data
-          .slice()
-          .filter((debitMemo: DebitMemo) => {
-            const searchStr = (
-              debitMemo.company +
-              debitMemo.returnMemoDate +
-              debitMemo.returnMemoNo +
-              debitMemo.returnMemoName 
-            ).toLowerCase();
-            return searchStr.indexOf(this.filter.toLowerCase()) !== -1;
-          });
-        // Sort filtered data
-        const sortedData = this.sortData(this.filteredData.slice());
-        // Grab the page's slice of the filtered sorted data.
-        const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
-        this.renderedData = sortedData.splice(
-          startIndex,
-          this.paginator.pageSize
-        );
-        return this.renderedData;
-      })
-    );
-  }
-  disconnect() {}
-  /** Returns a sorted copy of the database data. */
-  sortData(data: DebitMemo[]): DebitMemo[] {
-    if (!this._sort.active || this._sort.direction === "") {
-      return data;
-    }
-    return data.sort((a, b) => {
-      let propertyA: number | string = "";
-      let propertyB: number | string = "";
-      switch (this._sort.active) {
-        case "company":
-          [propertyA, propertyB] = [a.company, b.company];
-          break;
-        case "returnMemoDate":
-          [propertyA, propertyB] = [a.returnMemoDate, b.returnMemoDate];
-          break;
-        case "returnMemoNo":
-          [propertyA, propertyB] = [a.returnMemoNo, b.returnMemoNo];
-          break;
-        case "returnMemoName":
-          [propertyA, propertyB] = [a.returnMemoName, b.returnMemoName];
-          break;
-      }
-      const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
-      const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
-      return (
-        (valueA < valueB ? -1 : 1) * (this._sort.direction === "asc" ? 1 : -1)
-      );
-    });
   }
 }
