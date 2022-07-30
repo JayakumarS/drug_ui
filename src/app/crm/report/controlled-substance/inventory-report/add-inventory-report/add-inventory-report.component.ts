0d8c5f0a3b99916241c41d0f8ab8cd524e4abe3c
Riyaz Ahamed K <riyazahamed1444@gmail.com>
2022-07-30 15:15:04 +0530

    PDF

--- src/app/crm/report/controlled-substance/inventory-report/add-inventory-report/add-inventory-report.component.ts
+++ src/app/crm/report/controlled-substance/inventory-report/add-inventory-report/add-inventory-report.component.ts
@@ -249,42 +249,16 @@ export class AddInventoryReportComponent implements OnInit {
      this.hideFlag = false;
  }
   
-    //Export PDF
-   
-    // public openPDF(): void {
-    //   let DATA: any = document.getElementById('htmlData');
-    //   html2canvas(DATA).then((canvas) => {
-    //     let fileWidth = 208;
-    //     let fileHeight = (canvas.height * fileWidth) / canvas.width;
-    //     const FILEURI = canvas.toDataURL('image/png');
-    //     let PDF = new jsPDF('p', 'mm', 'a4');
-    //     let position = 0;
-    //     PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
-    //     PDF.save('ScheduleII.pdf');
-    //   });
-    // }
+    
 
-
-//     //Export PDF
+//Export PDF
     openPDF() {    
     
-    // this.entiymasterid = sessionStorage.getItem('entityId-usec'); 
     this.httpService.post<any>(this.inventoryformService.exportPDF, this.docForm.value).subscribe(
       (data) => {
-        // let file = new Blob([data], { type: "application/pdf" });
-        // var fileURL = URL.createObjectURL(file);
-        // window.open(fileURL);
-        // const doc = new jsPDF();
-    
-        // const pdfTable = this.pdfTable.nativeElement;
-        
-        // var html = htmlToPdfmake(pdfTable.innerHTML);
-          
-        // const documentDefinition = { content: html };
-        // pdfMake.createPdf(documentDefinition).open();
+     
         const doc = new jsPDF();
     
-    //const pdfTable = this.pdfTable.nativeElement;
     
     var html = htmlToPdfmake(data.exportPDF);
       
@@ -298,108 +272,6 @@ export class AddInventoryReportComponent implements OnInit {
  
 
 }
-
-
-exportPdf() {
-   
-  this.httpService.post<any>(this.inventoryformService.exportPDF, this.docForm.value).subscribe(
-    (data) => {
-  //     var str = removeFirstandLast(data.exportPDF);
-  //     str = str.substring(1, str.toString().length() - 1);
-  // html2canvas(str).then((canvas) => {
-  //       let fileWidth = 208;
-  //       let fileHeight = (canvas.height * fileWidth) / canvas.width;
-  //       const FILEURI = canvas.toDataURL('image/png');
-  //       let PDF = new jsPDF('p', 'mm', 'a4');
-  //       let position = 0;
-  //       PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
-  //       PDF.save('ScheduleII.pdf');
-  //     });
-
-  let newWin;
-  var datanew =  data.exportPDF.replace("<\"", '<').replace(">\"", '<');
-  var content = datanew.innerHTML;
-  var combined = document.createElement('div');
-  combined.innerHTML = content; 
-  combined.id = 'new';
-  newWin= window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
-  newWin.document.open();
-      newWin.document.write(`
-      <html>
-      <head>
-        <title>Print tab</title>            
-        <meta charset="utf-8">
-        <link rel="icon" type="image/x-icon" href="../favicion.png">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-        <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Base64/1.1.0/base64.min.js"></script>
-
-        <style type="text/css">
-          input {
-              outline: 0;
-              border-width: 0 0 2px;
-              width: 100px;
-          }
-    
-          @page {
-              size: auto;
-              margin: 5mm;
-          }
-          
-          @media print {
-            .col-md-1, .col-md-2, .col-md-3, .col-md-4, .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-md-10, .col-md-11, .col-md-12 {
-              float: left;
-          }
-          .col-md-12 {
-                width: 100%;
-          }
-          .col-md-11 {
-                width: 91.66666667%;
-          }
-          .col-md-10 {
-                width: 83.33333333%;
-          }
-          .col-md-9 {
-                width: 75%;
-          }
-          .col-md-8 {
-                width: 66.66666667%;
-          }
-          .col-md-7 {
-                width: 58.33333333%;
-          }
-          .col-md-6 {
-                width: 50%;
-          }
-          .col-md-5 {
-                width: 41.66666667%;
-          }
-          .col-md-4 {
-                width: 33.33333333%;
-          }
-          .col-md-3 {
-                width: 25%;
-          }
-          .col-md-2 {
-                width: 16.66666667%;
-          }
-          .col-md-1 {
-                width: 8.33333333%;
-          }
-          }
-      </style>
-    </head>
-    <body onload="window.print();window.close()">${combined.outerHTML}</body>
-    </html>`        
-       );
-  newWin.document.close();
-  
-}, error => {
-  console.log(error);
-  this.helper.errorMessage(error);
- });
-  }
 
 
 
